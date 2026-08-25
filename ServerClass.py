f4b3a4cd1bb43cb0318f88020585c6710e7f792b
14000560 <sharifian.b@entekhabgroup.com>
2026-08-25 16:02:35 +0330

    Add check_server function

--- ServerClass.py
+++ ServerClass.py
@@ -4,52 +4,55 @@ class Server:
         self.ip = ip
         self.cpu = cpu
         self.ram = ram
+    def check_server(self):
+        if self.cpu >=90 or self.ram>=90:
+             return "critical"
+        elif self.cpu>=70 or self.ram >=70:
+             return "warning"
+        else:
+             return "healthy"
+    def report(self):
 
-
-def check_server(Server):
-
-    if server.cpu >=90 or ram>=90:
-         return "critical"
-    elif cpu>=70 or ram >=70:
-       return "warning"
-    else:
-       return "healthy"
+        status = self.check_server()
+        print("=============")
+        print("server report")
+        print("==============")
+        print("hostname:", self.hostname)
+        print("ip:", self.ip)
+        print("cpu:", self.cpu)
+        print("ram:", self.ram)
+        print("status:", status)
 
 
 warning_count=0
 critical_count=0
 problem_servers=[]
 
-for server in Server:
-    status=check_server(server)
+cp1 = Server("cp1","10.0.0.1",50,65)
+cp2 = Server("cp2","10.0.0.2",80,90)
+servers=[cp1,cp2]
+
+for server in servers:
+    status = server.check_server()
 
     if status == "critical":
-        critical_count+=1
+        critical_count += 1
         problem_servers.append(server)
     elif status == "warning":
-        warning_count +=1
+        warning_count += 1
         problem_servers.append(server)
 
-    print("=============")
-    print("server report")
-    print("==============")
-    print("hostname:", hostname)
-    print("ip:", ip)
-    print("cpu:", cpu)
-    print("ram:", ram)
-    print("status:",status)
+    server.report()
 
 print("========== Summary ==========")
 print("Warning count:", warning_count)
 print("Critical count:", critical_count)
 print("Problem servers:")
 for server in problem_servers:
-    print(f"- {server['hostname']} (CPU: {server['cpu']}, RAM: {server['ram']})")
+    print(f"- hostname:{server.hostname} (CPU: {server.cpu}, RAM: {server.ram})")
 
 
-cp1 = Server("cp1","10.0.0.1",50,65)
-cp2 = Server("cp2","10.0.0.2",80,90)
 
-print(cp1.hostname)
-print(cp2.hostname)
-print(cp1.cpu)
+
+
+
